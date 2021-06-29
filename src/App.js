ee165b7a2063c2ab1a76bc89f7ca6d5126ea85ea
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2021-06-29 13:49:31 +1000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App"> Weather App </div>
+  return <div className="App"> 
+  <h1>Weather App</h1> 
+  <footer>
+    This code was created by Yue Ajioka and is{" "}
+    <a href="https://github.com/gocherrystar/react-weather-project" target="blank" >open-sourced on GitHub</a>
+  </footer>
+  </div>
 }
 
 
