461fc868e319977f245ced1946919a68c4246182
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2021-07-04 21:54:15 +1000

    Add animated weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -11,7 +11,7 @@ export default function Weather(props) {
         setWeatherData( {
         ready: true,
         temperature: response.data.main.temp,
-        iconUrl:`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+        icon:response.data.weather[0].icon,
         date: new Date(response.data.dt * 1000),
         description: response.data.weather[0].description,
        wind: response.data.wind.speed,
